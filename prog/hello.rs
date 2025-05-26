25a1cf0f5915e1d24c690d3c514c6e1f5d2ca948
novara754 <novara754@gmail.com>
2025-05-26 13:29:39 +0200

    small hello world demo program

---
+++ prog/hello.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+fn putc(c: u8) {
+    const OUT: *mut u32 = 0x1000_0000 as *mut u32;
+    unsafe { core::ptr::write_volatile(OUT, c as u32); }
+}
+
+fn puts(s: &[u8]) {
+    for &c in s {
+        putc(c);
+    }
+    putc(b'\n');
+}
+
+#[no_mangle]
+extern "C" fn main() {
+    puts(b"Hello, world!");
+}
+
+#[panic_handler]
+fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
